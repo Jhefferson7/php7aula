0ca3a340a1d8c83846ea82494d296b3da5e44410
jhefferson7 <jhefferson7@gmail.com>
2020-07-24 11:44:48 -0300

    classe usuario

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 
 require_once("config.php");
 
-$sql = new sql();
+// dar um select na tv usuarios e retornar na tela
+//$sql = new sql();
+//$usuarios = $sql->select("SELECT * from tb_usuarios");
+//echo json_encode($usuarios);
 
-$usuarios = $sql->select("SELECT * from tb_usuarios");
 
-echo json_encode($usuarios);
+$user = new Usuario();
+
+$user->loadById(1);
+
+echo $user;
+
+
+
+
 ?>
