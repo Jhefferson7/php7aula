dbab661ddc753e3db2361e928e73abc42121934f
jhefferson7 <jhefferson7@gmail.com>
2020-08-13 09:28:48 -0300

    METODOS DE LISTA

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 
 require_once("config.php");
 
-// dar um select na tv usuarios e retornar na tela
+// dar um select na tb_usuarios e retornar na tela
 //$sql = new sql();
 //$usuarios = $sql->select("SELECT * from tb_usuarios");
 //echo json_encode($usuarios);
 
 
-$user = new Usuario();
+//carrega um usuario pelo id
+//$user = new Usuario();
+//$user->loadById(1);
+//echo $user;
 
-$user->loadById(1);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $user;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("u");
+//echo json_encode($search);
 
 
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jheff", "7");
+echo $usuario;
 
 ?>
